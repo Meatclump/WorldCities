1e89902af5ab207d93ee8c1afc056bebeddc2a30
Meatclump <hepe22@hotmail.com>
2026-01-29 10:59:26 +0100

    Added City model

---
+++ WorldCities01/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldCities01.Models
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+    }
+}
